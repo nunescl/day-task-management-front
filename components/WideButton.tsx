a7e1761a6fea87a068c07ac43c3dac6c5c7f40b7
Ana Nunes <anaclaranunes@gmail.com>
2023-10-23 16:02:51 -0300

    remove console.log

--- components/WideButton.tsx
+++ components/WideButton.tsx
@@ -1,13 +1,8 @@
-import { ReactHTMLElement } from "react";
-
 const WideButton = (props: any) => {
-  console.log(props);
   return (
-    <div>
-      <button className="bg-[#FED36A] w-[85vw] h-16 font-bold	">
-        {props.children}
-      </button>
-    </div>
+    <button className="bg-[#FED36A] w-[85vw] h-16 font-bold	">
+      {props.children}
+    </button>
   );
 };
 
