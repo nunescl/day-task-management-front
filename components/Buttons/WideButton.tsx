fc0df2ca477a2189b230c27cc16c3ce605420974
Ana Nunes <anaclaranunes@gmail.com>
2023-11-03 10:32:35 -0300

    add function onClick to component

--- components/Buttons/WideButton.tsx
+++ components/Buttons/WideButton.tsx
@@ -1,6 +1,9 @@
 const WideButton = (props: any) => {
   return (
-    <button className="bg-[#FED36A] w-[85vw] h-16 font-bold	">
+    <button
+      className="bg-[#FED36A] w-[85vw] h-16 font-bold"
+      onClick={props.onClick}
+    >
       {props.children}
     </button>
   );
