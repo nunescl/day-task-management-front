ba910448578fa804f8983cddfbc8d96fe16ab3fb
Ana Nunes <anaclaranunes@gmail.com>
2023-11-03 10:35:03 -0300

    add type and router to /signin

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,56 @@
 /* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
 import WideButton from "@/components/Buttons/WideButton";
+import Head from "next/head";
+import { NextRouter, useRouter } from "next/router";
 
-export default function Home() {
+export default function Home(): React.ReactElement {
+  const router: NextRouter = useRouter();
+  const handleClick = (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    path: string
+  ) => {
+    e.preventDefault();
+    router.push(path);
+  };
+
   return (
-    <div className="flex flex-col items-center space-y-12 w-[85vw] mx-[7.5vw] my-[4vh]">
-      <Image
-        src="/logodtask.svg"
-        width={62}
-        height={93}
-        alt="Picture of the author"
-        className="self-start"
-      />
-      <div className="bg-white w-[85vw] flex justify-center">
+    <>
+      <Head>
+        <title>DayTask</title>
+      </Head>
+      <div className="flex flex-col items-center space-y-12 w-[85vw] mx-[7.5vw] my-[4vh]">
         <Image
-          src="/mandesk.svg"
-          width={320}
-          height={320}
+          src="/logodtask.svg"
+          width={62}
+          height={93}
           alt="Picture of the author"
-          className="self-center"
+          className="self-start"
         />
-      </div>
-      <div className="font-pilatDemi text-5xl w-[100%]">
-        <h1 className="text-[#FFFFFF]">
-          Manage <br /> your <br /> Task with
-        </h1>
-        <h1 className="text-[#FED36A]">DayTask</h1>
-      </div>
+        <div className="bg-white w-[85vw] flex justify-center">
+          <Image
+            src="/mandesk.svg"
+            width={320}
+            height={320}
+            alt="Picture of the author"
+            className="self-center"
+          />
+        </div>
+        <div className="font-pilatDemi text-5xl w-[100%]">
+          <h1 className="text-[#FFFFFF]">
+            Manage <br /> your <br /> Task with
+          </h1>
+          <h1 className="text-[#FED36A]">DayTask</h1>
+        </div>
 
-      <WideButton>Let's Start</WideButton>
-    </div>
+        <WideButton
+          onClick={(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) =>
+            handleClick(e, "/signin")
+          }
+        >
+          Let's Start
+        </WideButton>
+      </div>
+    </>
   );
 }
