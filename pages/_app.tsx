79f488de98c9d0bb3b3eb3ca8d39c48519161e57
Ana Nunes <anaclaranunes@gmail.com>
2023-11-03 10:32:57 -0300

    add type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,10 @@ import RootLayout from "@/app/layout";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { useEffect, useState } from "react";
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps): React.ReactElement {
   const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
